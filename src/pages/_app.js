66eb6b91a20ec173ea5cd861a6733ed47606f05f
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-04 12:20:31 -0300

    feat: Tentando mudar o icone do site na barra de cima

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,14 @@ import Header from '@/components/Header';
 import '../styles/globals.css';
 import '../styles/RootPage.css';
 import Aside from '@/components/Aside';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="images/placeholderImage.jpg" type="image/jpeg" />
+      </Head>
       <Header />
       <div className="layout-default">
         <Aside />
