8738475c1544de261ac2ba40cc2d82369f71c5e5
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-04 23:42:35 -0300

    feat: Usa o método getStaticProps para gerar as informações dos projetos durante o build da aplicação

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-function Projects() {
-  const fetchPow = async () => {
-    console.log(await(await fetch('http://localhost:3000/api/projects')).json());
-  };
-  
-  fetchPow();
+export async function getStaticProps() {
+  const { deployments } = await (await fetch('http://localhost:3000/api/projects')).json();
+  console.log(deployments);
+  return {
+    props: {
+      deployments,
+    },
+  }
+}
+
+function Projects({ deployments }) {
+  console.log(deployments);
   return (
     <div>
       Projects
+      {
+        deployments.map((project) => (
+          <p>{project.name}</p>
+        ))
+      }
     </div>
   );
 }
