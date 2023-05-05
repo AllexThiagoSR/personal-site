97d1841caf95e661225489a51975ecf3c730c21f
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-05 12:24:56 -0300

    feat: Fará requisição para pegar mais informações sobre o repositório do github

--- src/pages/api/projects/index.js
+++ src/pages/api/projects/index.js
@@ -1,5 +1,6 @@
 async function projects(_, response) {
   const token = process.env.ACCESS_API_TOKEN;
+  const GITHUB_BASE_URL = 'https://api.github.com/repos/AllexThiagoSR/';
   try {
     const data = await fetch('https://api.vercel.com/v6/deployments', {
       headers: {
@@ -7,6 +8,7 @@ async function projects(_, response) {
       },
       method: 'get',
     });
+    // const gitHubData = await fetch(GITHUB_BASE_URL);
     response.json(await data.json());
   } catch(e) {
     response.json({ message: 'Something went wrong' });
