d0597ca80e135b2654bf87173823b3027ec0d1c1
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-25 14:05:38 -0300

    Versão para deploy do site para desktops

--- src/pages/api/projects/index.js
+++ src/pages/api/projects/index.js
@@ -1,5 +1,6 @@
-async function projects(_, response) {
+async function GET(_, response) {
   const token = process.env.ACCESS_API_TOKEN;
+  const githubToken = process.env.GITHUB_API_TOKEN
   const GITHUB_BASE_URL = 'https://api.github.com/repos/AllexThiagoSR/';
   try {
     const data = await fetch('https://api.vercel.com/v6/deployments', {
@@ -9,16 +10,30 @@ async function projects(_, response) {
       method: 'get',
     });
     const { deployments } = await data.json();
-    let finalList = [];
-    for(const { name } of deployments) {
-      const { html_url, description, languages_url, homepage } = await( await fetch(GITHUB_BASE_URL + name)).json();
-      const languages = await (await fetch(languages_url)).json();
-      finalList = [...finalList, { name, url: homepage, githubUrl: html_url, description, languages }];
-    }
-    response.json(finalList);
+    const finalList = deployments.map(async({ name }) => {
+      const {
+        html_url,
+        description,
+        languages_url,
+        homepage,
+      } = await( await fetch(GITHUB_BASE_URL + name, {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+        method: 'get',
+      })).json();
+      const languages = await (await fetch(languages_url, {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+        method: 'get',
+      })).json();
+      return { name, url: homepage, githubUrl: html_url, description, languages };
+    });
+    response.json(await Promise.all(finalList));
   } catch(e) {
     response.json({ message: 'Something went wrong', errorMessage: e.message });
   }
 }
 
-export default projects;
+export default GET;
